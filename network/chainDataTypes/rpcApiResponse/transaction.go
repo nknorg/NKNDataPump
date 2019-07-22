0e4b9f0a909ae6ac90e733d3dfb4b210ad7f7e8f
elwin <elwinzhu@hotmail.com>
2019-07-22 13:42:07 -0700

    remove the comment of the old code

--- network/chainDataTypes/rpcApiResponse/transaction.go
+++ network/chainDataTypes/rpcApiResponse/transaction.go
@@ -1,59 +1,11 @@
 package rpcApiResponse
 
 
-//type TxAttributeInfo struct {
-//	Usage TransactionAttributeUsage
-//	Data  string
-//}
-//
-//type UTXOTxInputInfo struct {
-//	ReferTxID          string
-//	ReferTxOutputIndex int
-//}
-//
-//type BalanceTxInputInfo struct {
-//	AssetID     string
-//	Value       string
-//	ProgramHash string
-//}
-//
-//type TxoutputInfo struct {
-//	AssetID     string `json:"AssetID"`
-//	Value       string `json:"Value"`
-//	Address     string `json:"Address"`
-//	ProgramHash string `json:"ProgramHash"`
-//}
-//
-//type TxoutputMap struct {
-//	Key   string
-//	Txout []TxoutputInfo
-//}
-//
-//type AmountMap struct {
-//	Key   string
-//	Value string
-//}
-
 type ProgramInfo struct {
 	Code      string
 	Parameter string
 }
 
-//type Transaction struct {
-//	TxType         TransactionType      `json:"TxType"`
-//	PayloadVersion byte                 `json:"PayloadVersion"`
-//	Payload        interface{}          `json:"Payload"`
-//	Attributes     []TxAttributeInfo    `json:"Attributes"`
-//	UTXOInputs     []UTXOTxInputInfo    `json:"Inputs"`
-//	Outputs        []TxoutputInfo       `json:"Outputs"`
-//	Programs       []ProgramInfo        `json:"Programs"`
-//
-//	//AssetOutputs      []TxoutputMap `json:"AssetOutputs"`
-//	//AssetInputAmount  []AmountMap   `json:"AssetInputAmount"`
-//	//AssetOutputAmount []AmountMap   `json:"AssetOutputAmount"`
-//
-//	Hash string `json:"hash"`
-//}
 
 type Transaction struct {
 	TxType      string        `json:"TxType"`
--- task/chainDataPump/txProcessors.go
+++ task/chainDataPump/txProcessors.go
@@ -2,164 +2,13 @@ package chainDataPump
 
 import (
 	"NKNDataPump/storage/storageItem"
-	"NKNDataPump/network/chainDataTypes/rpcApiResponse/transactionPayload"
 	"NKNDataPump/common"
 	"encoding/hex"
 	"github.com/golang/protobuf/proto"
-	"NKNDataPump/storage"
 	"github.com/nknorg/nkn/pb"
 	"NKNDataPump/network/chainDataTypes/rpcApiResponse"
 	"strconv"
 )
-
-func commitProcessor(data interface{}, extData interface{}) (err error) {
-	txItem := extData.(storageItem.TransactionItem)
-	commitPayload := transactionPayload.Commit{}
-	err = common.JsonPointer2Struct(data, &commitPayload)
-
-	if nil != err {
-		common.Log.Fatal(err)
-		return
-	}
-
-	sigchain := &pb.SigChain{}
-	chainByte, _ := hex.DecodeString(commitPayload.SigChain)
-	err = proto.Unmarshal(chainByte, sigchain)
-
-	if nil != err {
-		common.Log.Error(err)
-		return
-	}
-
-	var sigchainItems []storageItem.IItem
-	for idx, v := range sigchain.Elems {
-		sig := &storageItem.SigchainItem{}
-		sig.MappingFrom(hex.EncodeToString(v.Signature), txItem)
-		//sig.Addr = hex.EncodeToString(v.Addr)
-		sig.NextPubkey = hex.EncodeToString(v.NextPubkey)
-		sig.SigIndex = uint32(idx)
-		sig.Timestamp = txItem.Timestamp
-
-		sigchainItems = append(sigchainItems, sig)
-	}
-
-	insertItems(sigchainItems)
-
-	return
-}
-
-//func transferProcessor(data interface{}, extData interface{}) (err error) {
-//	tx := data.(rpcApiResponse.Transaction)
-//	txItem := extData.(storageItem.TransactionItem)
-//
-//	currentUTXOOut := tx.Outputs
-//	refUTXO := tx.UTXOInputs
-//
-//	//check current utxo
-//	//currentUTXOCount := len(currentUTXOOut)
-//	//if currentUTXOCount > 2 {
-//	//	common.Log.Fatalf("not normal nkn utxo model in block %d!!", txItem.Height)
-//	//	os.Exit(0)
-//	//}
-//
-//	if 0 == len(refUTXO) {
-//		common.Log.Error("no ref utxo shown!!")
-//		os.Exit(0)
-//	}
-//
-//	//get ref utxos
-//	var refUTXOList []rpcApiResponse.TxoutputInfo
-//
-//	for _, v := range refUTXO {
-//		utxo, err := getRefUTXO(v.ReferTxID, v.ReferTxOutputIndex)
-//		if nil != err {
-//			//todo: error process
-//			common.Log.Errorf("some thing wrong when try to get ref utxos."+
-//				"err [%v] . block height [%d] . tx hash [%s]", err, txItem.Height, txItem.Hash)
-//			return err
-//		}
-//
-//		if "" == utxo.Address {
-//			utxo.Address, err = common.ScriptHashToAddress(utxo.ProgramHash)
-//
-//			if nil != err {
-//				//todo: error process
-//				common.Log.Tracef("some thing wrong when calc address."+
-//					"err [%v] . block height [%d] . tx hash [%s]", err, txItem.Height, txItem.Hash)
-//				return err
-//			}
-//		}
-//
-//		refUTXOList = append(refUTXOList, utxo)
-//
-//		go recordAddr(utxo.Address, txItem)
-//	}
-//
-//	//calc the transfer
-//	txItem.AssetId = currentUTXOOut[0].AssetID
-//	transferItems := utxoTransferCalc(refUTXOList, currentUTXOOut, txItem)
-//	if nil != transferItems {
-//		err = insertItems(transferItems)
-//	}
-//
-//	return
-//}
-
-func registerAssetPayloadProcessor(data interface{}, extData interface{}) (err error) {
-	txItem := extData.(storageItem.TransactionItem)
-	regPayload := transactionPayload.Register{}
-	err = common.JsonPointer2Struct(data, &regPayload)
-
-	assetItem := storageItem.AssetItem{}
-	assetItem.MappingFrom(regPayload, txItem)
-	txItem.AssetId = assetItem.Hash
-
-	insertItems([]storageItem.IItem{
-		&assetItem,
-	})
-
-	return
-}
-
-func assetIssueProcessor(data interface{}, extData interface{}) (err error) {
-	issueItem := storageItem.AssetIssueItem{}
-	issueItem.MappingFrom(data, extData)
-
-	storage.IgnoreKeyInsert([]storageItem.IItem{
-		&issueItem,
-	})
-
-	insertItems([]storageItem.IItem{
-		&issueItem,
-	})
-
-	go recordAddr(issueItem.IssueTo, extData.(storageItem.TransactionItem))
-
-	return
-}
-
-//func payProcessor(data interface{}, extData interface{}) (err error) {
-//	tx := data.(rpcApiResponse.Transaction)
-//	txItem := extData.(storageItem.TransactionItem)
-//	payPayload := transactionPayload.Pay{}
-//	common.JsonPointer2Struct(tx.Payload, &payPayload)
-//
-//	var payList []storageItem.IItem
-//
-//	for _, o := range tx.Outputs {
-//		newPayItem := &storageItem.PayItem{}
-//		newPayItem.MappingFrom(payPayload, txItem)
-//
-//		newPayItem.Payee = o.Address
-//		newPayItem.Value = o.Value
-//
-//		payList = append(payList, newPayItem)
-//	}
-//
-//	err = insertItems(payList)
-//
-//	return
-//}
 
 func coinbaseProcessor(data interface{}, extData interface{}) (err error) {
 	tx := data.(rpcApiResponse.Transaction)
@@ -215,14 +64,6 @@ func sigchainProcessor(data interface{}, extData interface{}) (err error) {
 		common.Log.Error(err)
 		return
 	}
-
-	//somehash := hex.EncodeToString(sigchain.BlockHash)
-	//nonce := sigchain.Nonce
-	//dataSize := sigchain.DataSize
-	//srcId := hex.EncodeToString(sigchain.SrcId)
-	//srcPubKey := hex.EncodeToString(sigchain.SrcPubkey)
-	//destId := hex.EncodeToString(sigchain.DestId)
-	//destPubKey := hex.EncodeToString(sigchain.DestPubkey)
 
 	var sigchainItems []storageItem.IItem
 	for i, v := range sigchain.Elems {
