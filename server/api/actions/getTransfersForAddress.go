182fddb3f459c03dd4e68fc3fd3f178d71cae8bd
heron <peng.liu@nkn.org>
2021-06-07 18:39:45 +0800

    add log

--- server/api/actions/getTransfersForAddress.go
+++ server/api/actions/getTransfersForAddress.go
@@ -1,15 +1,14 @@
 package apiServerAction
 
 import (
+	"github.com/gin-gonic/gin"
 	. "github.com/nknorg/NKNDataPump/common"
 	. "github.com/nknorg/NKNDataPump/server/api/const"
 	"github.com/nknorg/NKNDataPump/server/api/response"
 	"github.com/nknorg/NKNDataPump/storage/dbHelper"
-	"github.com/gin-gonic/gin"
 )
 
-var GetTransferForAddress IRestfulAPIAction = &getTransferForAddress{
-}
+var GetTransferForAddress IRestfulAPIAction = &getTransferForAddress{}
 
 type getTransferForAddress struct {
 	restfulAPIBase
@@ -21,18 +20,18 @@ func (g *getTransferForAddress) URI(serverURI string) string {
 
 func (g *getTransferForAddress) Action(ctx *gin.Context) {
 	defer func() {
-		if r:=recover(); nil != r {
+		if r := recover(); nil != r {
 			Log.Error(r)
 		}
 	}()
-
+	Log.Info("api: /transfer/address")
 	response := apiServerResponse.New(ctx)
 	addr := ""
 	page := uint64(0)
 
-	paramMap := map[string]interface{} {
+	paramMap := map[string]interface{}{
 		LOWERCASE_WORD_ADDRESS: &addr,
-		LOWERCASE_WORD_PAGE:      &page,
+		LOWERCASE_WORD_PAGE:    &page,
 	}
 
 	err := g.getUrlParam(paramMap, ctx)
@@ -40,17 +39,22 @@ func (g *getTransferForAddress) Action(ctx *gin.Context) {
 		response.BadRequest(nil)
 		return
 	}
+	Log.Infof("params, addr: %s, page: %d", addr, page)
 
+	Log.Info("query start.")
 	transferCount, _ := dbHelper.QueryTransferCountForAddr(addr)
 	transferList, _, err := dbHelper.QueryTransferForAddr(addr, page)
+
+	Log.Info("query end.")
+
 	if nil != err {
 		Log.Error(err)
 		response.InternalServerError(nil)
 		return
 	}
 
-	response.Success(map[string] interface{}{
-		"list": transferList,
+	response.Success(map[string]interface{}{
+		"list":  transferList,
 		"count": transferCount,
 	})
 }
